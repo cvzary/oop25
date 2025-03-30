af2607ebd7b3f54f4adfbf54663acc4659c13c3d
macbookair <cezary.rvd@gmail.com>
2025-03-30 20:12:25 +0200

    poprawki, skonczone lab4

---
+++ decorator_example/src/CinnamonDecorator.java
@@ -0,0 +1,11 @@
+public class CinnamonDecorator extends BeverageDecorator {
+
+    public CinnamonDecorator(Beverage decoratedBeverage) {
+        super(decoratedBeverage);
+    }
+
+    @Override
+    public String serve() {
+        return super.serve() + " + cynamon";
+    }
+}
