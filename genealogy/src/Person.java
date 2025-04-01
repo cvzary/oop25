3f14b812a87d8af033d82d803b43a30472385ecf
macbookair <cezary.rvd@gmail.com>
2025-04-02 01:15:20 +0200

    skonczono lab5

---
+++ genealogy/src/Person.java
@@ -0,0 +1,64 @@
+import java.time.LocalDate;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Person implements Comparable<Person> {
+    private final String name, surname;
+    private final LocalDate birth;
+    private final Set<Person> children;
+
+    public Person(String name, String surname, LocalDate birth) {
+        this.name = name;
+        this.surname = surname;
+        this.birth = birth;
+        this.children = new HashSet<>();
+    }
+
+
+    public boolean adopt(Person child) {
+        if(child == this)
+            return false;
+        return children.add(child);
+    }
+
+    public Person getYoungestChild() {
+        if(children.isEmpty())
+            return null;
+        return Collections.max(children);
+
+//        return children.stream().max(Person::compareTo).get();
+//        Person youngest = null;
+//        for(Person child : children) {
+//            if(youngest == null || child.birth.isAfter(youngest.birth))
+//                youngest = child;
+//        }
+//        return youngest;
+    }
+
+    public List<Person> getChildren() {
+        return children.stream()
+                .sorted()
+                .toList();
+    }
+
+    public String getFullName() {
+        return name + " " + surname;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", birth=" + birth + '}';
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return this.birth.compareTo(o.birth);
+    }
+
+}
